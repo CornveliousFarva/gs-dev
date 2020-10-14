10cf3e3a4aa30cab34c1567cf09526a8e8a95e18
Greg Schuman <gcschuman@hotmail.com>
2020-10-13 20:56:33 -0700

    Added content to items in the components folder.

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,34 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl'
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
-
-class App extends Component{
-  render(){
-    return(
-      <div style={{height: '300px', position: 'relative'}}>
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
     <Layout style={{background: 'url(https://visitoceanside.org/wp-content/uploads/2015/03/IMG_0882-1024x683-e1426545133243.jpg) center / cover'}}>
-        <Header transparent title="Title" style={{color: 'white'}}>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Greg Schuman</Link>} scroll>
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/about">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
-        <Drawer title="Title">
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Greg Schuman</Link>}>
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+              <Link to="/resume">Resume</Link>
+              <Link to="/about">About Me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
             </Navigation>
         </Drawer>
-        <Content />
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
     </Layout>
 </div>
-    )
+
+    );
   }
 }
 
 export default App;
+
