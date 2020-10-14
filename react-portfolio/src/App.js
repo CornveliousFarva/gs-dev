ba5826ed74096ee771edc61b1b20d4ef0e495b7a
Greg Schuman <gcschuman@hotmail.com>
2020-10-13 19:27:11 -0700

    Updated the app.js.

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -4,9 +4,27 @@ import './App.css';
 class App extends Component{
   render(){
     return(
-      <div>
-        <h1>Do The Thing</h1>
-      </div>
+      <div style={{height: '300px', position: 'relative'}}>
+    <Layout style={{background: 'url(https://visitoceanside.org/wp-content/uploads/2015/03/IMG_0882-1024x683-e1426545133243.jpg) center / cover'}}>
+        <Header transparent title="Title" style={{color: 'white'}}>
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content />
+    </Layout>
+</div>
     )
   }
 }
